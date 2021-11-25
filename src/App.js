7a431ce65bbb4b324f2771f0d6ddf5acd611e29e
luisluft <luberluft@gmail.com>
2021-11-25 10:24:27 -0400

    refactor async code from the component to the store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { sendCartData } from "./store/cart-slice";
 
 import Notification from "./components/UI/Notification";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 
 let isFirstPageLoad = true;
 
@@ -16,47 +16,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending",
-          message: "sending cart data",
-        })
-      );
-
-      const response = await fetch(
-        "https://luft-react-shopping-cart-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) throw new Error("Sending cart data failed.");
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully.",
-        })
-      );
-
-      const responseData = await response.json();
-    };
-
     if (isFirstPageLoad) {
       isFirstPageLoad = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed.",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
